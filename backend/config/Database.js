35b13313318204f1543bd64f0c65c2412d8a4165
jericokatong <jericokatong@gmail.com>
2023-02-25 22:43:37 +0800

    mengatur timezone reading sequelize from database

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -4,6 +4,12 @@ import sequelize from "sequelize";
 const db = new sequelize("crud_db", "root", "", {
   host: "localhost",
   dialect: "mysql",
+  dialectOptions: {
+    useUTC: false,
+    dateStrings: true,
+    typeCast: true,
+  },
+  timezone: "+08:00",
 });
 
 export default db;
